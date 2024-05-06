34bd6071edc3a986c4544cefda4aa5db1b095afc
Joey Fung <joeyfcc0525@gmail.com>
2024-05-06 14:58:16 +0100

    Initial commits

--- web.py
+++ web.py
@@ -18,18 +18,16 @@ st.write('Create and organise your to-do items')
 
 # Add a new key which can identify each to-do item
 # in the session_state dict
-# Add index to identify items to be deleted from the list
 for index, todo in enumerate(todos):
     # checkbox contains True/False value
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
         functions.write_todo(todos)
-        # Also delete the item from the FE
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label='label', placeholder='Add new to-do'
+st.text_input(label='label', placeholder='Add new to-do' 
             , on_change=add_todo, key='new_todo')
 
 # st.session_state
